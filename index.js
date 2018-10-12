a4e3dbcd99f5bd243768cf9d82dadd2f390dc5a3
Haoda Wang <haodawan@usc.edu>
2018-10-12 13:39:18 -0700

    add tanh activation function

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var Koa = require('koa');
 var Router = require('koa-router');
 var sigmoid = require('./sigmoid');
+var tanh = require('./tanh');
 
 var app = new Koa();
 var router = new Router();
 
-router.post('/', (ctx, next) => {
+router.post('/sigmoid', (ctx, next) => {
   ctx.body = { sigmoid: sigmoid(ctx.request.body.x) };
+});
+
+router.post('/tanh', (ctx, next) => {
+	ctx.body = { sigmoid: tanh(ctx.request.body.x) };
 });
 
 app
